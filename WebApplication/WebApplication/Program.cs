753711bd00839d97242198548b10c1c909e9caff
Artorioss <102064880+Artorioss@users.noreply.github.com>
2025-07-22 21:38:45 +0300

    Настроил cors (разрешил обращаться к нашему приложению всем подряд)

--- WebApplication/WebApplication/Program.cs
+++ WebApplication/WebApplication/Program.cs
@@ -1,4 +1,4 @@
-using Infrastructure.EntityFramework;
+﻿using Infrastructure.EntityFramework;
 using Infrastructure.Repositories.ImplementationInfrastructure.EntityFrameworks;
 using Infrastructure.Repositories.Implementations;
 using Microsoft.EntityFrameworkCore;
@@ -22,6 +22,18 @@ builder.Services.AddScoped<DocRepository>();
 builder.Services.AddScoped<PotentRepository>();
 builder.Services.AddScoped<CompetitionRepository>();
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy
+            //  ✅ Разрешаем любой localhost / 127.0.0.1 на любых портах
+            .SetIsOriginAllowed(origin => new Uri(origin).IsLoopback)
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials();
+    });
+});
 
 builder.Services.AddOpenApiDocument(options =>
 {
@@ -40,6 +52,7 @@ else
     app.UseHsts();
 }
 
+app.UseCors();
 app.UseOpenApi();
 app.UseSwaggerUi(x =>
 {
@@ -59,7 +72,7 @@ using (var scope = app.Services.CreateScope())
     var db = scope.ServiceProvider.GetRequiredService<Context>();
     await db.Database.EnsureDeletedAsync();
     await db.Database.EnsureCreatedAsync();
-    //await db.Database.MigrateAsync(); //TODO: ����� �� �������� :(
+    //await db.Database.MigrateAsync(); //TODO: Метод не работает :(
     await DbInitializer.InitializeAsync(db);
 }
 
